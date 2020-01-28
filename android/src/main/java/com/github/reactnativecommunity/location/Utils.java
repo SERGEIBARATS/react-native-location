d9d383a59c685a1150b4974a8ce2de1c2bd56981
SERGEIBARATS <38927319+SERGEIBARATS@users.noreply.github.com>
2020-01-28 17:31:03 +0200

    Update Utils.java

--- android/src/main/java/com/github/reactnativecommunity/location/Utils.java
+++ android/src/main/java/com/github/reactnativecommunity/location/Utils.java
@@ -2,7 +2,9 @@ package com.github.reactnativecommunity.location;
 
 import android.location.Location;
 import android.os.Build;
-import android.support.annotation.Nullable;
+//import android.support.annotation.Nullable;
+
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
